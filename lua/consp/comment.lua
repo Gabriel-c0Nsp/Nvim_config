c5194e989b0d8f828875a8365f1a9424ab358fff
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-08-29 09:56:43 -0300

    adicionado plugin que mostra status (referentes ao Git) de repositórios com .git na raiz

--- lua/consp/comment.lua
+++ lua/consp/comment.lua
@@ -23,7 +23,7 @@ comment.setup {
         ---Line-comment keymap
         line = 'gc',
         ---Block-comment keymap
-        block = 'gb',
+        block = 'g*',
     },
     ---LHS of extra mappings
     extra = {
--- lua/consp/plugins.lua
+++ lua/consp/plugins.lua
@@ -91,6 +91,9 @@ use "windwp/nvim-autopairs"
 -- Comments
 use "numToStr/Comment.nvim"
 
+-- Gitsigns
+use "lewis6991/gitsigns.nvim"
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
