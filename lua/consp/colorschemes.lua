2bd20251993d843f04bb51d8e3604ccadd465648
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-16 00:42:06 -0300

    Configuração do tema agora permanece ao retornar para o neovim

--- lua/consp/colorschemes.lua
+++ lua/consp/colorschemes.lua
@@ -1,40 +1,43 @@
--- catch de possível erro ao tentar utilizar um tema não existente:
+local function save_current_colorscheme(theme)
+    local config_file = io.open(vim.fn.expand("~/.config/nvim/colorscheme_config"), "w")
+    config_file:write(theme)
+    config_file:close()
+end
 
---local colorscheme = "colorschemeHere"
-
---local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-
---if not status_ok then
---  vim.notify("Tema(colorscheme) " .. colorscheme .. "não encontrado!")
---  return
---end
-
+local function load_saved_colorscheme()
+    local config_file = io.open(vim.fn.expand("~/.config/nvim/colorscheme_config"), "r")
+    if config_file then
+        local theme = config_file:read("*line")
+        if theme and theme ~= "" then
+            vim.cmd("colorscheme " .. theme)
+        end
+        config_file:close()
+    end
+end
 
 local opts = { noremap = true, silent = true }
-local term_opts = { silent = true }
-
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
 
 local colorschemes = {
-  "default",
-  "zellner"
+    "default",
+    "zellner"
 }
 
 local current_colorscheme = 1
 
 function change_colorscheme()
-  current_colorscheme = current_colorscheme % #colorschemes + 1
-
-  vim.cmd("colorscheme " .. colorschemes[current_colorscheme])
-  vim.cmd("hi SignColumn guibg=None")
-  print("Changing color to: " .. colorschemes[current_colorscheme])
+    current_colorscheme = current_colorscheme % #colorschemes + 1
+    local new_theme = colorschemes[current_colorscheme]
+    vim.cmd("colorscheme " .. new_theme)
+    vim.cmd("hi SignColumn guibg=None")
+    print("Changing color to: " .. new_theme)
+    save_current_colorscheme(new_theme)
 end
 
+local keymap = vim.api.nvim_set_keymap
 keymap('n', '<A-c>', ':lua require"consp.colorschemes".change_colorscheme()<CR>', opts)
 
-return {
-  change_colorscheme = change_colorscheme
-}
+load_saved_colorscheme()
 
--- TODO: fazer com que a configuração seja lembrada mesmo após sair
+return {
+    change_colorscheme = change_colorscheme
+}
