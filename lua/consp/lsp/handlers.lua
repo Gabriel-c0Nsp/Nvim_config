0a473bd6f0c22edef691918c24f3d90cf088e5b1
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-10-02 00:42:51 -0300

    Removido sintax highlighting para java (eu particularmente não curtia), provido através do CMP lsp

--- lua/consp/lsp/handlers.lua
+++ lua/consp/lsp/handlers.lua
@@ -43,7 +43,6 @@ M.setup = function()
     border = "rounded",
   })
 end
-
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
   if client.server_capabilities.documentHighlight then
@@ -69,7 +68,7 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-*>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader><leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(
@@ -85,8 +84,9 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
+  if client.name == "jdtls" or clien.name == "java" then
     client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.semanticTokensProvider = nil
   end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
