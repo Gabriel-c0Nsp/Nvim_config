eff783a7cf110446c4c30fec0e5f033c0b6a210b
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-04-10 09:37:09 -0300

    Atualização na funcionalidade backspace

--- lua/consp/backspace.lua
+++ lua/consp/backspace.lua
@@ -4,9 +4,17 @@ M.backspace = function()
   local word = vim.fn.expand("<cword>")
   local first_char = string.sub(word, 1, 1)
 
-  if word == "" or first_char == "\"" or first_char == "'" or first_char == "]" or first_char == ")" or first_char == "\\" then
+  if word == "" or
+    first_char == "\"" or
+    first_char == "'" or
+    first_char == "]" or
+    first_char == ")" or
+    first_char == "}" or
+    first_char == ">" or
+    first_char == "\\" then
+
     vim.cmd("normal! b")
-    -- word = vim.fn.expand("<cword>")
+    -- word = vim.fn.expand("<cword>") -- para fins de debug
   end
 
   vim.cmd("normal! diw")
